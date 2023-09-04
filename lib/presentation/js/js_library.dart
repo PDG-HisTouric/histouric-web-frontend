b7c5d9ab6ad95f7eeb5e96967414c818fdd57945
Juan Torres <378tech@gmail.com>
2023-09-04 14:59:31 -0500

    added javascript test code and changed the datasources endpoints

---
+++ lib/presentation/js/js_library.dart
@@ -0,0 +1,18 @@
+@JS()
+library script.js;
+
+import 'package:js/js.dart';
+
+@JS()
+external String getPlatform();
+
+// This function will do Promise to return something
+@JS()
+external dynamic jsPromiseFunction(String message);
+
+// This function will open new popup window for given URL.
+@JS()
+external dynamic jsOpenTabFunction(String url);
+
+///         ^            ^                ^
+///      return     functionName       arguments
