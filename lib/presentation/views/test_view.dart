ec16d321485c1c93045c3420cd19099622c43e04
Juan Torres <378tech@gmail.com>
2023-09-08 04:45:46 -0500

    load an image from a drive url using its id

--- lib/presentation/views/test_view.dart
+++ lib/presentation/views/test_view.dart
@@ -1,8 +1,11 @@
-import 'package:dio/dio.dart';
+import 'package:universal_html/html.dart';
+
 import 'package:flutter/material.dart';
 
 import '../../config/constants/constants.dart';
 import '../js_bridge/js_bridge.dart';
+
+import 'dart:ui_web' as ui;
 
 class TestView extends StatelessWidget {
   const TestView({super.key});
@@ -29,27 +32,36 @@ class TestView extends StatelessWidget {
           },
         ),
         const SizedBox(height: 20),
-        ElevatedButton(
-          child: const Text(
-            "Hacer petición",
-            style: TextStyle(color: Colors.black),
-          ),
-          onPressed: () async {
-            final dio = Dio(BaseOptions(
-              baseUrl:
-                  'https://drive.google.com/uc?id=1Uj27zjdTO3nfejVPV9GLBP_eAfBBJEM6',
-            ));
-            final response = await dio.get('');
-            print(response);
-          },
-        ),
-        ConstrainedBox(
-          constraints: const BoxConstraints(maxHeight: 200),
-          child: Image.network(
-            'https://lh3.googleusercontent.com/d/1Uj27zjdTO3nfejVPV9GLBP_eAfBBJEM6',
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: SizedBox(
+            width: 400,
+            height: 400,
+            child: FittedBox(
+              fit: BoxFit.cover,
+              child: SizedBox(height: 853, width: 1280, child: HtmlImage()),
+            ),
           ),
         ),
       ],
     ));
+  }
+}
+
+class HtmlImage extends StatelessWidget {
+  HtmlImage({super.key}) {
+    ui.platformViewRegistry.registerViewFactory('image', (int viewId) {
+      var image = ImageElement(
+        src: 'https://drive.google.com/uc?id=1Uj27zjdTO3nfejVPV9GLBP_eAfBBJEM6',
+      );
+      image.style.height = '100%';
+      image.style.width = '100%';
+      return image;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const HtmlElementView(viewType: 'image');
   }
 }
