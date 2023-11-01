6d7c46a6a60931482b188bdae6c2be429788fbac
Juan Pablo Ramos Torres <378tech@gmail.com>
2023-11-01 12:53:04 -0500

    Removed needsUrlGen attribute from HistoryImage

--- lib/domain/entities/history_image.dart
+++ lib/domain/entities/history_image.dart
@@ -2,14 +2,12 @@ class HistoryImage {
   final String id;
   final String imageUri;
   final int startTime;
-  final bool needsUrlGen;
   final String historyId;
 
   HistoryImage({
     required this.id,
     required this.imageUri,
     required this.startTime,
-    required this.needsUrlGen,
     required this.historyId,
   });
 }
