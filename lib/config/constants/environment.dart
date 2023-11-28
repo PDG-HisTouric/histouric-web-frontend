525abf7d7861ab18de23230982eab71e6bb5c523
Juan Pablo Ramos Torres <378tech@gmail.com>
2023-11-28 15:18:59 -0500

    Unnecessary environment variables removed

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -10,24 +10,4 @@ class Environment {
       const String.fromEnvironment('APP_ID', defaultValue: 'APP_ID not found');
   static String baseUrl = const String.fromEnvironment('BASE_URL',
       defaultValue: 'BASE_URL not found');
-  static String directionsApiKey = const String.fromEnvironment(
-      'DIRECTIONS_API_KEY',
-      defaultValue: 'DIRECTIONS_API_KEY not found');
-  static String firebaseApiKey = const String.fromEnvironment(
-      'FIREBASE_API_KEY',
-      defaultValue: 'FIREBASE_API_KEY not found');
-  static String firebaseAppId = const String.fromEnvironment('FIREBASE_APP_ID',
-      defaultValue: 'FIREBASE_APP_ID not found');
-  static String firebaseMessagingSenderId = const String.fromEnvironment(
-      'FIREBASE_MESSAGING_SENDER_ID',
-      defaultValue: 'FIREBASE_MESSAGING_SENDER_ID not found');
-  static String firebaseProjectId = const String.fromEnvironment(
-      'FIREBASE_PROJECT_ID',
-      defaultValue: 'FIREBASE_PROJECT_ID not found');
-  static String firebaseAuthDomain = const String.fromEnvironment(
-      'FIREBASE_AUTH_DOMAIN',
-      defaultValue: 'FIREBASE_AUTH_DOMAIN not found');
-  static String firebaseStorageBucket = const String.fromEnvironment(
-      'FIREBASE_STORAGE_BUCKET',
-      defaultValue: 'FIREBASE_STORAGE_BUCKET not found');
 }
